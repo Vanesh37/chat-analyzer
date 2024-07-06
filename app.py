3a3b0f7d5db24a9c5e142307d10beac8fee0a5f3
Vanesh37 <110464597+Vanesh37@users.noreply.github.com>
2024-07-06 22:36:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ import streamlit as st
 import whatsapp
 import telegram
 
-
+st.set_page_config(page_title="WhatsApp Chat Analyzer")
 st.sidebar.title("Chat Analyser")
 
 app_list = ['WhatsApp', 'Telegram']
 selected_app = st.sidebar.selectbox("Select app", app_list)
 
 if selected_app == 'WhatsApp':
-    st.set_page_config(page_title="WhatsApp Chat Analyzer")
     whatsapp.whatsapp()
 else:
-    st.set_page_config(page_title="Telegram Chat Analyzer")
     telegram.telegram()
